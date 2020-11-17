64bcde203bd5350dfa0d1cc9e9d8eed96c8ac57a
Sol Proc <info@kp2r.network>
2020-11-18 00:32:30 +0600

    CI included

--- src/stores/config/config.js
+++ src/stores/config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  provider: 'https://mainnet.infura.io/v3/211c5f35ed97405b86465c1d76502922',
+  provider: 'https://mainnet.infura.io/v3/5d0a70d46f2a4f9ba579cf04645f25f0',
   // unioracleAddress: '0x127a2975c4E1c75f1ed4757a861bbd42523DB035',
   keep3rOracleAddress: '0x73353801921417f465377c8d898c6f4c0270282c'
 };
